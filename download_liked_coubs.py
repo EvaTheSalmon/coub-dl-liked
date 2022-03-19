a2750df6ffcfd73758384734dbde965c7e6ebb07
Роман Беседин <besedin-r-i@yandex.ru>
2022-03-19 08:21:42 +0000

    API token support, improved portability

--- download_liked_coubs.py
+++ download_liked_coubs.py
@@ -1,84 +1,104 @@
 import os
-from coub_api import CoubApi
 import urllib
+import urllib.request
+import aiohttp
 import soundfile as sf
-import pandas as pd
-
-# use CoubApi
-api = CoubApi()
+import json
+import traceback
+import asyncio
+import subprocess
 
 # https://coub.com/api/v2/users/me
+api_token = os.getenv("API_TOKEN")
+if api_token is None:
+    print("API_TOKEN environment variable must be specified")
+    exit(1)
 
-""" Couldnt get it to work with my access token
-os.environ.get("your access token can be found in above link")
-api.authenticate("your access token can be found in above link")
-api.timeline.me_liked()
-"""
-# the last line gives me HTTPError: 403 Client Error: Forbidden for url: https://coub.com/api/v2/timeline/likes?page=1&per_page=10&access_token=....
-# if somebody can fix the above that would be ideal... 
-# but I didn't have time to do it so did it manually with steps below
+async def get_page_as_json(session, i):
+    async with session.get(f"https://coub.com/api/v2/timeline/likes?page={i}&per_page=25&api_token={api_token}") as response:
+        return await response.json()
 
-# 1. go to https://coub.com/api/v2/timeline/likes?page=1&per_page=25
-# 2. copy each page (e.g., page=1,page=2,page=...) from above to mylikes.txt file as a new line (I put 2 pages of my old likes as demo)
-# 3. run code below
+async def main():
+    # async fetch all "pages" with videos info
+    async with aiohttp.ClientSession() as session:
+        total_pages = (await get_page_as_json(session, 1))['total_pages']
+        print(f"Total page count: {total_pages}")
+        print("Fetching all pages...")
+        pages = await asyncio.gather(*(get_page_as_json(session, i) for i in range(1, total_pages+1)))
+        print(f"{len(pages)} pages fetched")
+   
+    # save fetched info
+    with open('all_likes.json', 'w') as outfile:
+        json.dump(pages, outfile)
 
-with open('./mylikes.txt', encoding="utf8") as f:
-    likes = f.readlines()
+    coubs = []
+    for i in range(len(pages)):
+        coubs = coubs + pages[i]['coubs']
 
-# awkwardly find IDs from the coub api data structure... 
-ALL_IDs = []
-for i in range(len(likes)):
-    print(i)
-    for j  in likes[i].split(':'):
-        if j.split(',')[0].isnumeric():
-            try:
-                if j.split(',')[1] == '"type"': # this should filter your liked video IDs only
-                    ALL_IDs.append(j.split(',')[0])
-            except:
-                pass
-# save IDs as .csv - might be useful to save if the above takes a long time 
-pd.DataFrame(ALL_IDs).to_csv('ALL_IDs.csv')
+    print(f'Total COUB\'s video count: {len(coubs)}')
 
-# load up IDs
-ALL_IDs = pd.read_csv('./ALL_IDs.csv') 
+    proceed = input("Proceed to download. y/n (y) ")
+    if len(proceed) != 0 and proceed.lower() != 'y':
+        exit()
 
-# filter short IDs which are not actually videos
-IDs = []
-for i in ALL_IDs.to_numpy()[:,1]: # use 1st col
-    if i not in IDs and len(str(i)) >= 8: # >= IDs len 8 and 9 are the ones with my liked videos
-        IDs.append(i)
+    # download liked videos to dir videos/
+    for i, coub in enumerate(coubs):
+        try:
+            id = coub['permalink']
+            print("-" * 24)
+            print(f"Downloading video {i+1}, permalink: {id}")
 
-# download liked videos to dir videos/
-for id in IDs:
-    try:
-        # use coub API to get target coub
-        coub = api.coubs.get_coub(str(id))
+            mp3_url = coub['file_versions']['html5']['audio']['high']['url']
+            mp3_fln = mp3_url.split('/')[-1]
+            video_url = coub['file_versions']['html5']['video']['high']['url']
+            video_fln = video_url.split('/')[-1]
+            
+            urllib.request.urlretrieve(mp3_url, mp3_fln)
+            urllib.request.urlretrieve(video_url, video_fln)
 
-        # get audio from coub API
-        mp3_url = coub.file_versions.dict()['html5']['audio']['high']['url'].split()[0]
-        mp3_fln = coub.file_versions.dict()['html5']['audio']['high']['url'].split()[0].split('/')[-1]
-        video_url = coub.file_versions.dict()['html5']['video']['high']['url'].split()[0]
-        video_fln = coub.file_versions.dict()['html5']['video']['high']['url'].split()[0].split('/')[-1]
+            out_video_fln = f'{i+1}_{id}.mp4'
+            out_video_fln_tmp = f'{id}_tmp.mp4'
+            out_wav_fln = f'{id}.wav'
 
-        # download mp3 and mp4 files from coub.com
-        urllib.request.urlretrieve(mp3_url, mp3_fln)	
-        urllib.request.urlretrieve(video_url, video_fln)	
+            # convert mp3 to wav
+            subprocess.run(['ffmpeg', '-i', mp3_fln, '-vn', '-acodec', 'pcm_s16le', 
+                '-ac', '2', '-ar', '44100', '-f', 'wav', out_wav_fln]).check_returncode()
+            # read wav and get file len in seconds
+            x, sr = sf.read(f'{out_wav_fln}')
+            wav_len = len(x)/sr
+            # loop video to duration of file len
+            subprocess.run(['ffmpeg', '-stream_loop', '-1', '-t', str(wav_len), 
+                '-i', video_fln, '-c', 'copy', out_video_fln_tmp]).check_returncode()
+            
+            # combine MP3 with looped video, add metadata
+            channel_title = coub['channel']['title']
+            channel_permalink = coub['channel']['permalink']
+            tags = []
+            for tag in coub['tags']:
+                tags.append(tag['title'])
+            tags_str = ';'.join(tags)
+            comment = 'Author: %s\nLink: %s\nOriginal video: %s\nTags: %s' % (
+                    channel_title,
+                    f'https://coub.com/{channel_permalink}',
+                    f'https://coub.com/view/{id}',
+                    tags_str)
+            title = coub['title']
+            subprocess.run(["ffmpeg", "-i", out_video_fln_tmp, "-i", mp3_fln, 
+                                    "-metadata", "title=%s" % title,
+                                    "-metadata", "comment=%s" % comment,
+                                    "-c:v", "copy", "-c:a", "aac", f"videos/{out_video_fln}"]).check_returncode()
+            # remove temp files
+            os.remove(out_video_fln_tmp)
+            os.remove(out_wav_fln)
+            os.remove(mp3_fln)
+            os.remove(video_fln)
+        except Exception as e:
+            print("-" * 24)
+            print(traceback.format_exc())
+            proceed = input("Caught some errors, proceed downloading? y/n (y) ")
+            if len(proceed) != 0 and proceed.lower() != 'y':
+                exit()
+            pass
 
-        # filenames
-        out_video_fln = f'{id}.mp4'
-        out_video_fln_tmp = f'{id}_tmp.mp4'
-        out_wav_fln = f'{id}.wav'
-
-        # convert mp3 to wav
-        os.system(f'ffmpeg -i {mp3_fln} -vn -acodec pcm_s16le -ac 2 -ar 44100 -f wav {out_wav_fln}')
-        # read wav and get file len in seconds
-        x, sr = sf.read(f'{out_wav_fln}')
-        wav_len = len(x)/sr
-        # loop video to duration of file len
-        os.system(f'ffmpeg -stream_loop -1 -t {wav_len} -i {video_fln} -c copy {out_video_fln_tmp}')
-        # combine MP3 with looped video
-        os.system(f'ffmpeg -i {out_video_fln_tmp} -i {mp3_fln} -c:v copy -c:a aac videos/{out_video_fln}')
-        # remove temp files
-        os.system(f'rm {out_video_fln_tmp} {out_wav_fln} {mp3_fln} {video_fln}')
-    except: # nothing should go wrong but if it does ignore it
-        pass
+if __name__ == '__main__':
+    asyncio.run(main())
