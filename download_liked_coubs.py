9aba66e352b9617d857a060f606ac9cc88d263ca
Роман Беседин <besedin-r-i@yandex.ru>
2022-03-20 01:31:49 +0000

    print -> logging, ignore already processed coubs

--- download_liked_coubs.py
+++ download_liked_coubs.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import urllib
 import urllib.request
@@ -7,68 +8,99 @@ import json
 import traceback
 import asyncio
 import subprocess
+import logging
+import sys
 
-# https://coub.com/api/v2/users/me
-api_token = os.getenv("API_TOKEN")
-if api_token is None:
-    print("API_TOKEN environment variable must be specified")
-    exit(1)
+PAGES_DUMP_JSON_FILENAME = 'likes.json'
 
-async def get_page_as_json(session, i):
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)-5.5s]  %(message)s",
+    handlers=[
+        logging.FileHandler(f"{datetime.now().strftime('%d-%b-%Y %H_%M_%S')}.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+async def get_likes_page_as_json(session, i, api_token):
+    logging.debug(f"Fetching page {i}")
     async with session.get(f"https://coub.com/api/v2/timeline/likes?page={i}&per_page=25&api_token={api_token}") as response:
         return await response.json()
 
-async def main():
-    # async fetch all "pages" with videos info
+async def save_likes_pages():
+    # https://coub.com/api/v2/users/me
+    api_token = os.getenv("API_TOKEN")
+    if api_token is None:
+        sys.exit("API_TOKEN environment variable must be specified")
+    # async fetch all "pages" with liked videos info
     async with aiohttp.ClientSession() as session:
-        total_pages = (await get_page_as_json(session, 1))['total_pages']
-        print(f"Total page count: {total_pages}")
-        print("Fetching all pages...")
-        pages = await asyncio.gather(*(get_page_as_json(session, i) for i in range(1, total_pages+1)))
-        print(f"{len(pages)} pages fetched")
-   
+        total_pages = (await get_likes_page_as_json(session, 1, api_token))['total_pages']
+        logging.info(f"Total page count: {total_pages}")
+        logging.info("Fetching all pages...")
+        pages = await asyncio.gather(*(get_likes_page_as_json(session, i, api_token) for i in range(1, total_pages+1)))
+        logging.info(f"{len(pages)} pages fetched")
     # save fetched info
-    with open('all_likes.json', 'w') as outfile:
-        json.dump(pages, outfile)
+    with open(PAGES_DUMP_JSON_FILENAME, 'w') as f:
+        json.dump(pages, f)
+    logging.info(f"COUB's info dumped to a file")
+
+def get_coubs_from_likes_pages_dump():
+    with open(PAGES_DUMP_JSON_FILENAME, 'r') as f:
+        pages = json.load(f)
+    logging.info(f"COUB's info loaded from a file")
 
     coubs = []
     for i in range(len(pages)):
         coubs = coubs + pages[i]['coubs']
+    logging.info(f'Total COUB\'s video count: {len(coubs)}')
+    return coubs
 
-    print(f'Total COUB\'s video count: {len(coubs)}')
+def delete_file_if_exists(filepath):
+    if os.path.exists(filepath):
+        os.remove(filepath)
+
+async def main():
+    if not os.path.exists(PAGES_DUMP_JSON_FILENAME):
+        await save_likes_pages()
+
+    coubs = get_coubs_from_likes_pages_dump()
 
     proceed = input("Proceed to download. y/n (y) ")
     if len(proceed) != 0 and proceed.lower() != 'y':
-        exit()
-
+        exit(0)
+    
     # download liked videos to dir videos/
     for i, coub in enumerate(coubs):
         try:
             id = coub['permalink']
-            print("-" * 24)
-            print(f"Downloading video {i+1}, permalink: {id}")
+            logging.info(f"Downloading video {i+1}, permalink: {id}")
+
+            out_video_fpath = os.path.join('videos', f'{id}.mp4')
+            out_video_fname_tmp = f'{id}_tmp.mp4'
+            out_wav_fname = f'{id}.wav'
+
+            video_url = coub['file_versions']['html5']['video']['high']['url']
+            video_fname = video_url.split('/')[-1]
 
             mp3_url = coub['file_versions']['html5']['audio']['high']['url']
-            mp3_fln = mp3_url.split('/')[-1]
-            video_url = coub['file_versions']['html5']['video']['high']['url']
-            video_fln = video_url.split('/')[-1]
-            
-            urllib.request.urlretrieve(mp3_url, mp3_fln)
-            urllib.request.urlretrieve(video_url, video_fln)
+            mp3_fname = mp3_url.split('/')[-1]
+ 
+            if os.path.exists(out_video_fpath):
+                logging.info(f"{out_video_fpath} already exists")
+                continue
 
-            out_video_fln = f'{i+1}_{id}.mp4'
-            out_video_fln_tmp = f'{id}_tmp.mp4'
-            out_wav_fln = f'{id}.wav'
+            urllib.request.urlretrieve(mp3_url, mp3_fname)
+            urllib.request.urlretrieve(video_url, video_fname)
 
             # convert mp3 to wav
-            subprocess.run(['ffmpeg', '-i', mp3_fln, '-vn', '-acodec', 'pcm_s16le', 
-                '-ac', '2', '-ar', '44100', '-f', 'wav', out_wav_fln]).check_returncode()
+            subprocess.run(['ffmpeg', '-i', mp3_fname, '-vn', '-acodec', 'pcm_s16le', 
+                '-ac', '2', '-ar', '44100', '-f', 'wav', out_wav_fname], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).check_returncode()
             # read wav and get file len in seconds
-            x, sr = sf.read(f'{out_wav_fln}')
+            x, sr = sf.read(f'{out_wav_fname}')
             wav_len = len(x)/sr
             # loop video to duration of file len
             subprocess.run(['ffmpeg', '-stream_loop', '-1', '-t', str(wav_len), 
-                '-i', video_fln, '-c', 'copy', out_video_fln_tmp]).check_returncode()
+                '-i', video_fname, '-c', 'copy', out_video_fname_tmp], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).check_returncode()
             
             # combine MP3 with looped video, add metadata
             channel_title = coub['channel']['title']
@@ -83,22 +115,18 @@ async def main():
                     f'https://coub.com/view/{id}',
                     tags_str)
             title = coub['title']
-            subprocess.run(["ffmpeg", "-i", out_video_fln_tmp, "-i", mp3_fln, 
+            subprocess.run(["ffmpeg", "-i", out_video_fname_tmp, "-i", mp3_fname, 
                                     "-metadata", "title=%s" % title,
                                     "-metadata", "comment=%s" % comment,
-                                    "-c:v", "copy", "-c:a", "aac", f"videos/{out_video_fln}"]).check_returncode()
-            # remove temp files
-            os.remove(out_video_fln_tmp)
-            os.remove(out_wav_fln)
-            os.remove(mp3_fln)
-            os.remove(video_fln)
+                                    "-c:v", "copy", "-c:a", "aac", out_video_fpath], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).check_returncode()
         except Exception as e:
-            print("-" * 24)
-            print(traceback.format_exc())
-            proceed = input("Caught some errors, proceed downloading? y/n (y) ")
-            if len(proceed) != 0 and proceed.lower() != 'y':
-                exit()
-            pass
+            logging.error(f'Failed to process video {id}')
+            logging.error(traceback.format_exc())
+        finally:
+            delete_file_if_exists(out_video_fname_tmp)
+            delete_file_if_exists(video_fname)
+            delete_file_if_exists(out_wav_fname)
+            delete_file_if_exists(mp3_fname)
 
 if __name__ == '__main__':
     asyncio.run(main())
