2e386c41345d03e41f2bf4215eeb783f6ce62ff2
EvaTheSalmon <18466855+EvaTheSalmon@users.noreply.github.com>
2024-06-12 00:13:37 +0300

    Updated structure and refactoring

--- check_consistency.py
+++ check_consistency.py
@@ -2,71 +2,60 @@ import os
 import sys
 import json
 import logging
-
 from datetime import datetime
 
+class DirectoryScanner:
+    def __init__(self):
+        self._setup_logging()
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)-5.5s]  %(message)s",
-    handlers=[
-        logging.FileHandler(f"logs/{datetime.now().strftime('%d-%b-%Y %H_%M_%S')}.log"),
-        logging.StreamHandler(sys.stdout),
-    ],
-)
+    def _setup_logging(self):
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s [%(levelname)-5.5s]  %(message)s",
+            handlers=[
+                logging.FileHandler(f"logs/{datetime.now().strftime('%d-%b-%Y %H_%M_%S')}.log"),
+                logging.StreamHandler(sys.stdout),
+            ],
+        )
 
+    def load_names_from_directory(self, directory: str) -> dict:
+        file_names = {}
+        for _, _, files in os.walk(directory):
+            for file in files:
+                file_names.add(file)
+        return file_names
 
-def scan_directory(directory: str) -> list:
-    file_names = []
-    for _, _, files in os.walk(directory):
-        for file in files:            
-            file_names.add(file)
-    return file_names
+    def load_names_from_json(self, json_file: str) -> dict:
+        if os.path.exists(json_file):
+            with open(json_file, 'r') as f:
+                try:
+                    return set(json.load(f))
+                except json.JSONDecodeError:
+                    return {}
+        return {}
+        
+    def compare_json_and_file_structure(self, json_data: dict, directory_structure_data: dict) -> bool:
+        return not bool(json_data - directory_structure_data)
+    
+    def check_if_file_in_json(self, file_name: str, json_dat: dict) -> bool:
+        if file_name in json_data:
+            return True
+        else:
+            return False
+    
+    def save_names_to_json(self, file_names: dict, json_path: str) -> None:
+        with open(json_path, 'w') as f:
+            json.dump(file_names, f, indent=4)
 
-
-def save_names_to_json(file_names: list, json_file: str):
-    with open(json_file, 'w') as f:
-        json.dump(file_names, f, indent=4)
-
-
-def load_names_from_json(json_file: str) -> dict:
-    if os.path.exists(json_file):
-        with open(json_file, 'r') as f:
-            try:
-                return json.load(f)
-            except json.JSONDecodeError:
-                return {}
-    return {}
-
-
-def main() -> None:
-    directory = "videos"
-    json_file = "file_names.json"
-
-    current_names = scan_directory(directory)
-
-    if not check_consistency_file_name_list(current_names, "file_names.json"):
-        save_names_to_json(current_names, json_file)
-        logging.info(f"Inconsistency found. Data was updated")
-    logging.info(f"All files consistent.")
 
 if __name__ == "__main__":
-    main()
-
-
-def check_if_duplicate(file_name: str, name_list: dict) -> bool:
-    if file_name in name_list:
-        return True
-    else:
-        return False
+    scanner = DirectoryScanner()
     
+    directory_structure_data = scanner.load_names_from_directory("videos")
+    json_data                = scanner.load_names_from_json("videos_dump.json")
 
-def check_consistency_file_name_list(current_names: list, json_file: str) -> bool:
-    """Checks if all files in video folder present in file"""
-
-    saved_names = load_names_from_json(json_file)
-
-    for file_name in current_names.items():
-        if file_name not in saved_names:
-            return False
-    return True
+    if scanner.compare_json_and_file_structure(json_data, directory_structure_data):
+        logging.info("Data is consistent")
+    else:
+        scanner.save_names_to_json(directory_structure_data, "videos_dump.json")
+        logging.info("Inconsitency found, data was updated")
